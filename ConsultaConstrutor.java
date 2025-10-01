18b53b88050a8e55c5d8efa0bdf4b0ca62c4e506
nayaraporto <naya08.vitoria@gmail.com>
2025-10-01 09:18:37 -0300

    Update ConsultaConstrutor.java

--- ConsultaConstrutor.java
+++ ConsultaConstrutor.java
@@ -1,22 +1,21 @@
 package Exercicios;
 
-import java.util.Date;
-
 	public class ConsultaConstrutor {
 
 	    public static void main(String[] args) {
 	        
-	        Date hoje = new Date(); 
+	        String hoje = new String(); 
 	        
 	        Consulta consulta1 = new Consulta(hoje);
-	        Consulta consulta2 = new Consulta("Maria", "Dr. João");
-	        Consulta consulta3 = new Consulta(hoje, "Carlos", "Dra. Ana");
+	        Consulta consulta2 = new Consulta("Nayara", "Dr. Murillo");
+	        Consulta consulta3 = new Consulta(hoje, "Noah", "Dra. Ana");
 
-	        System.out.println("Consulta 1 - Data: " + consulta1.getData());
+	        System.out.println("Consulta 1 - Data: 01/10/2025" + consulta1.getData());
 	        System.out.println("Consulta 2 - Paciente: " + consulta2.getNomePcente() + " | Dentista: " + consulta2.getNomeDents());
-	        System.out.println("Consulta 3 - Data: " + consulta3.getData() + " | Paciente: " + consulta3.getNomePcente() + " | Dentista: " + consulta3.getNomeDents());
+	        System.out.println("Consulta 3 - Data: 02/10/2025 " + consulta3.getData() + " | Paciente: " + consulta3.getNomePcente() + " | Dentista: " + consulta3.getNomeDents());
 	    }
 	}
+
 
 
 
